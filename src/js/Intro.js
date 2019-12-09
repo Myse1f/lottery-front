3deb6daf7c95e0a53f038c6a398daccfb5b3f87e
Myse1f <3150101170@zju.edu.cn>
2019-12-09 09:42:15 +0800

    open link in a new tab

--- src/js/Intro.js
+++ src/js/Intro.js
@@ -13,9 +13,9 @@ class Intro extends Component {
                 <Container className="Intro">
                     <Row>
                         <Col md={{span: 6, offset:3}}>
-                            <p>This Lottery Game is based on Ethereum. Contract address is <a href="https://ropsten.etherscan.io/address/0x4B693E745eAaa995aA70c4e10F807d1a6f689A9b" className="App-link">0x4B693E745eAaa995aA70c4e10F807d1a6f689A9b</a>.</p>
+                            <p>This Lottery Game is based on Ethereum. Contract address is <a href="https://ropsten.etherscan.io/address/0x4B693E745eAaa995aA70c4e10F807d1a6f689A9b" target="_blank" className="App-link">0x4B693E745eAaa995aA70c4e10F807d1a6f689A9b</a>.</p>
                             <p>
-                                You need install <a href="https://metamask.io/" class="App-link">MetaMask</a> first if you want to participate in this game. Every bet requires 0.1 ether.
+                                You need to install <a href="https://metamask.io/" target="_blank" class="App-link">MetaMask</a> first if you want to participate in this game. Every bet requires 0.1 ether.
                                 As soon as players reach 11, we randomly choose a winner 10 blocks in the future, to get 1 ether for reward.
                             </p>
                         </Col>
