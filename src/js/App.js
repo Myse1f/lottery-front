9937a41ca93d19593cf9f65a711d193843d2cfe4
Myse1f <3150101170@zju.edu.cn>
2019-12-09 17:26:00 +0800

    handle bet

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import '../css/App.css';
-import TruffleContract from '@truffle/contract'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Web3 from 'web3';
 import Intro from './Intro'
@@ -15,14 +14,13 @@ class App extends Component {
       lottery: undefined,
       numOfPlayers: 0,
       threshold: 11,
+      winner: '',
     };
     this.initWeb3 = this.initWeb3.bind(this);
-    this.initContract = this.initContract.bind(this);
   }
 
   componentWillMount() {
     this.initWeb3();
-    this.initContract();
   }
 
   async initWeb3() {
@@ -51,25 +49,12 @@ class App extends Component {
     App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
 
     const web3 = new Web3(App.web3Provider);
+    this.setState({
+      web3Provider: App.web3Provider
+    })
     const account = await web3.eth.getCoinbase();
     this.setState({ 
       account: account,
-      web3: web3
-    });
-  }
-
-  async initContract() {
-    var contract = TruffleContract({
-      abi: [{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"settlementBlockNumber","type":"uint256"}],"name":"Drawing","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"winner","type":"address"}],"name":"Drawn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"player","type":"address"}],"name":"Play","type":"event"},{"constant":false,"inputs":[],"name":"play","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"draw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"internalType":"address payable[]","name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getSettlementBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getThreshold","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"}],
-    });
-    contract.setProvider(App.web3Provider);
-    const lottery = await contract.at("0x4CAeD04F3841204e7AB110D1f1Bd5A1b60555EFc");
-    const players = await lottery.getPlayers();
-    const threshold = await lottery.getThreshold();
-    this.setState({
-      lottery: lottery,
-      numOfPlayers: players ? players.length : 0,
-      threshold: threshold,
     });
   }
 
@@ -82,7 +67,10 @@ class App extends Component {
           </p>
         </header>
         <Intro/>
-        <Lottery numOfPlayers={this.state.numOfPlayers} threshold={this.state.threshold} account={this.state.account} winner='0x123'/>
+        <Lottery 
+          web3Provider={this.state.web3Provider}
+          account={this.state.account} 
+          winner={this.state.winner}/>
         <footer className="App-footer"><small>&copy; Copyright 2019, Myse1f</small></footer>
       </div>
     );
--- src/js/Lottery.js
+++ src/js/Lottery.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import TruffleContract from '@truffle/contract'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -8,16 +9,54 @@ import '../css/Lottery.css'
 
 class Lottery extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleBet = this.handleBet.bind(this);
+        this.state = {
+            numOfPlayers: 0,
+            threshold: 0,
+        }
+    }
+
+    async componentDidMount() {
+        var contract = TruffleContract({
+            abi: [{"inputs":[],"payable":true,"stateMutability":"payable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"settlementBlockNumber","type":"uint256"}],"name":"Drawing","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"winner","type":"address"}],"name":"Drawn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"player","type":"address"}],"name":"Play","type":"event"},{"constant":false,"inputs":[],"name":"play","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":false,"inputs":[],"name":"draw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"internalType":"address payable[]","name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getSettlementBlock","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getThreshold","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"}],
+        });
+        contract.setProvider(this.props.web3Provider);
+        const lottery = await contract.at("0x4CAeD04F3841204e7AB110D1f1Bd5A1b60555EFc");
+        const players = await lottery.getPlayers();
+        const threshold = await lottery.getThreshold();
+        this.setState({
+            lottery: lottery,
+            numOfPlayers: players ? players.length : 0,
+            threshold: threshold,
+        });
+    }
+
+    async handleBet() {
+        const lottery = this.props.lottery;
+        const res = await lottery.play({from: this.props.account, value: 10**17});
+        for (var i=0; i<res.logs.length; i++){
+            switch (res.logs[i].event) {
+                case 'Play': 
+                    
+                    break;
+                case 'Drawing': break;
+                default: break;
+            }
+        }
+    }
+
     render() {
         return (
             <Container className="Lottery">
                 <Row>
                     <Col md={{span: 8, offset:2}}>
                         <div className="account">Your Account: {this.props.account}</div>
-                        <ProgressBar animated label={`${this.props.numOfPlayers}/${this.props.threshold}`} 
-                            now={this.props.numOfPlayers/this.props.threshold * 100} />
+                        <ProgressBar animated label={`${this.state.numOfPlayers}/${this.state.threshold}`} 
+                            now={this.state.numOfPlayers/this.state.threshold * 100} />
                         <div className="button">
-                            <Button variant="outline-danger" size="lg">Bet</Button>
+                            <Button onClick={this.handleBet} variant="outline-danger" size="lg">Bet</Button>
                         </div>
                             {this.props.winner !== '' && <div className="winner">winner: {this.props.winner}
                         </div>}
